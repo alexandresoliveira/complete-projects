9954a69f7812adc4a4799a1e7b44608fd269536b
Alexandre Salvador de Oliveira <alexandresalvadoroliveiradev@gmail.com>
2020-04-20 19:46:42 -0300

    Move project to another repository

--- ToDo/backend/src/main/java/dev/aleoliv/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.aleoliv.repository;
-
-import dev.aleoliv.models.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
--- ToDo/backend/src/main/resources/db/migration/V0.0.1__CREATE_TASKS.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `tasks` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `title` VARCHAR(30) NOT NULL,
-  `description` VARCHAR(100) NOT NULL,
-  `created_at` TIMESTAMP NOT NULL,
-  `updated_at` TIMESTAMP NOT NULL,
-  PRIMARY KEY (`id`)
-);
